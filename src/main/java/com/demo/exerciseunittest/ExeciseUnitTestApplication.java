492d78d695188a1be720876f91e5965b12f5ede1
samuel.funck <159692435+samuelFNK@users.noreply.github.com>
2025-08-28 12:30:05 +0200

    *test comment to confirm correct setup

--- src/main/java/com/demo/exerciseunittest/ExeciseUnitTestApplication.java
+++ src/main/java/com/demo/exerciseunittest/ExeciseUnitTestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExeciseUnitTestApplication {
 
+    //my test comment
     public static void main(String[] args) {
         SpringApplication.run(ExeciseUnitTestApplication.class, args);
     }
